2ae0fac729a841c993f3c96c13b4e58d76862c31
yurii-corssa <yurii.corssa@gmail.com>
2023-09-16 13:59:48 +0200

    Integrate auth persistence in store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,29 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { filterReducer } from './filterSlice';
 import { contactsReduser } from './contactsSlice';
+import { authPersistReducer, authReducer } from './auth/authSlice';
+import {
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 
 export const store = configureStore({
   reducer: {
+    auth: authPersistReducer,
     contacts: contactsReduser,
-    filter: filterReducer,
+    // filter: filterReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
