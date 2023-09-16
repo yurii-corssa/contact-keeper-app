1ef42a1d9ce0e26e94adbe769861a9acec1c85b3
yurii-corssa <yurii.corssa@gmail.com>
2023-09-16 15:38:46 +0200

    Added react-helmet; commented in Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import { Flex } from '@chakra-ui/react';
 import Greeting from 'components/Greeting';
 import RegistrationForm from 'components/RegistrationForm';
+import { Helmet } from 'react-helmet';
 import { Outlet } from 'react-router-dom';
 
 const Home = () => {
   return (
     <Flex height="100vh" alignItems="center" justifyContent="space-evenly">
+      {/* <Helmet>
+        <title>Home</title>
+      </Helmet> */}
       <Greeting />
       <Outlet />
     </Flex>
