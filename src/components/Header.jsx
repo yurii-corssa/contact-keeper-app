bf52a7a9d97998dbc5c8011337c1d6b7d1d1148a
yurii-corssa <yurii.corssa@gmail.com>
2023-09-18 19:05:52 +0200

    Refactor Header with dynamic tabs and routing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,43 @@
-import { Box, Flex, Tab, TabList, Tabs } from '@chakra-ui/react';
-import { Link } from 'react-router-dom';
+import { Flex, Tab, TabList, Tabs } from '@chakra-ui/react';
+import { Link, useLocation } from 'react-router-dom';
 import UserMenu from './UserMenu';
+import { selectIsLoggedIn } from 'redux/auth/auth-selectors';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  const location = useLocation();
+
+  const getTabIndex = path => {
+    switch (path) {
+      case '/':
+        return 0;
+      case '/contacts':
+        return 1;
+      case '/auth/login':
+        return 1;
+      case '/auth/sign-up':
+        return 2;
+
+      default:
+        return 0;
+    }
+  };
+
+  const tabIndex = getTabIndex(location.pathname);
+
   return (
-    <Tabs as="header">
+    <Tabs as="header" index={tabIndex}>
       <Flex as={TabList} justifyContent="space-between" alignItems="center">
         <Flex>
-          <Link to="/login">
+          <Link to="/">
             <Tab>Home</Tab>
           </Link>
-          <Link to="/contacts">
-            <Tab>Contacts</Tab>
-          </Link>
+          {isLoggedIn && (
+            <Link to="/contacts">
+              <Tab>Contacts</Tab>
+            </Link>
+          )}
         </Flex>
         <UserMenu />
       </Flex>
