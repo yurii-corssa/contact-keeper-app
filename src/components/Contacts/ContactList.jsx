97a27e7b008c87d603b5a569283bf66bb1cdbf8a
yurii-corssa <yurii.corssa@gmail.com>
2023-09-20 22:46:20 +0200

    Refactor ContactList with Redux and fetching

--- src/components/Contacts/ContactList.jsx
+++ src/components/Contacts/ContactList.jsx
@@ -1,13 +1,44 @@
-import { Flex, Heading } from '@chakra-ui/react';
+import { Box, Flex, Heading } from '@chakra-ui/react';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  selectIsLoggedIn,
+  selectIsRefreshing,
+  selectToken,
+} from 'redux/auth/auth-selectors';
+import { getContactsThunk } from 'redux/contacts/contacts-operations';
+import {
+  selectContacts,
+  selectFilter,
+  selectVisibleContacts,
+} from 'redux/contacts/contacts-selectors';
+import { ContactItems } from './ContactItems';
 
-const ContactList = () => {
+const ContactsList = () => {
+  //   const isLoggedIn = useSelector(selectIsLoggedIn);
+  //   const isRefreshing = useSelector(selectIsRefreshing);
+  //   const contacts = useSelector(selectContacts);
+  //   const filterValue = useSelector(selectFilter);
+  //   const filteredContacts = useSelector(selectVisibleContacts);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getContactsThunk());
+  }, [dispatch]);
+
+  //   console.log(contacts);
+
   return (
-    <Flex direction="column" p={14} width="100%">
-      <Heading size="xl" mb={10}>
+    <Flex direction="column" width="100%">
+      {/* <Heading size="xl" mb={10}>
         Contacts
-      </Heading>
+      </Heading> */}
+      <Box maxHeight="100vh" overflowY="auto">
+        <ContactItems />
+      </Box>
     </Flex>
   );
 };
 
-export default ContactList;
+export default ContactsList;
--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -1,16 +1,15 @@
 import { Flex } from '@chakra-ui/react';
-import { ContactForm } from 'components/Contacts/ContactForm';
-import ContactList from 'components/Contacts/ContactList';
+import ContactsList from 'components/Contacts/ContactList';
 import ContactTools from 'components/Contacts/ContactTools';
 
 const ContactsPage = () => {
   return (
-    <Flex height="100vh" alignItems="center" justifyContent="space-evenly">
+    <Flex height="100vh">
       {/* <Helmet>
         <title>Home</title>
       </Helmet> */}
       <ContactTools />
-      <ContactList />
+      <ContactsList />
     </Flex>
   );
 };
