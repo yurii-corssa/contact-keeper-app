72ef4ee3df0b0458fd2d6596d5655a38a0155b84
yurii-corssa <yurii.corssa@gmail.com>
2023-09-23 00:19:07 +0200

    Modularize Input Fields

--- src/components/Contacts/ContactForm.jsx
+++ src/components/Contacts/ContactForm.jsx
@@ -2,44 +2,31 @@ import { useDispatch, useSelector } from 'react-redux';
 import { selectContacts } from 'redux/contacts/contacts-selectors';
 import { normalizeStr } from 'utils/normalizeStr';
 import { addContactThunk } from 'redux/contacts/contacts-operations';
-import { Field, Form, Formik } from 'formik';
-import { PhoneIcon } from '@chakra-ui/icons';
-import { FaUserAlt } from 'react-icons/fa';
-import {
-  Button,
-  Flex,
-  FormControl,
-  FormErrorMessage,
-  FormLabel,
-  Icon,
-  Input,
-  InputGroup,
-  InputLeftElement,
-  Stack,
-} from '@chakra-ui/react';
+import { Form, Formik } from 'formik';
+import { Button, Flex, Stack, useToast } from '@chakra-ui/react';
+import ContactNameInput from 'components/Inputs/ContactNameInput';
+import ContactNumberInput from 'components/Inputs/ContactNumberInput';
 
-export const ContactForm = () => {
+export const ContactForm = ({ initialValues = { name: '', number: '' } }) => {
   const contacts = useSelector(selectContacts);
-
   const dispatch = useDispatch();
+  const toast = useToast();
+
+  // const initialValues = { name: '', number: '' };
 
   const isContact = value =>
     contacts.find(({ name }) => normalizeStr(name) === normalizeStr(value));
 
-  const validateName = value => {
-    if (!value) {
-      return 'Name is required';
-    }
-  };
-  const validateNumber = value => {
-    if (!value) {
-      return 'Number is required';
-    }
-  };
-
   const handleSubmit = ({ name, number }, actions) => {
     if (isContact(name)) {
-      alert(`${name} is already to contacts`);
+      toast({
+        title: 'Duplicate Contact',
+        description: `"${name}" is already in contacts.`,
+        status: 'error',
+        duration: 5000,
+        isClosable: true,
+        position: 'top',
+      });
       actions.setSubmitting(false);
       return;
     }
@@ -49,49 +36,12 @@ export const ContactForm = () => {
   };
 
   return (
-    <Flex direction="column" p={14} width="100%">
-      <Formik initialValues={{ name: '', number: '' }} onSubmit={handleSubmit}>
+    <Flex direction="column" paddingX="14" paddingY="5" width="100%">
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
         {props => (
           <Stack as={Form} gap={3}>
-            <Field type="text" name="name" validate={validateName}>
-              {({ field, form }) => (
-                <FormControl isInvalid={form.errors.name && form.touched.name}>
-                  <FormLabel>Name</FormLabel>
-                  <InputGroup>
-                    <InputLeftElement pointerEvents="none">
-                      <Icon as={FaUserAlt} color="gray.300" />
-                    </InputLeftElement>
-                    <Input
-                      {...field}
-                      variant="filled"
-                      placeholder="Contact Name"
-                    />
-                  </InputGroup>
-
-                  <FormErrorMessage>{form.errors.name}</FormErrorMessage>
-                </FormControl>
-              )}
-            </Field>
-            <Field type="tel" name="number" validate={validateNumber}>
-              {({ field, form }) => (
-                <FormControl
-                  isInvalid={form.errors.number && form.touched.number}
-                >
-                  <FormLabel>Number</FormLabel>
-                  <InputGroup>
-                    <InputLeftElement pointerEvents="none">
-                      <PhoneIcon color="gray.300" />
-                    </InputLeftElement>
-                    <Input
-                      {...field}
-                      variant="filled"
-                      placeholder="Phone number"
-                    />
-                  </InputGroup>
-                  <FormErrorMessage>{form.errors.name}</FormErrorMessage>
-                </FormControl>
-              )}
-            </Field>
+            <ContactNameInput />
+            <ContactNumberInput />
             <Button
               textColor="fff"
               colorScheme="blue"
