e3881a862b13192ce39463074fbac28aa9fa0fd1
yurii-corssa <yurii.corssa@gmail.com>
2023-09-23 00:20:29 +0200

    Modularize Input Fields and Enhance Validation

--- src/components/Auth/RegistrationForm.jsx
+++ src/components/Auth/RegistrationForm.jsx
@@ -1,90 +1,83 @@
-import { Field, Form, Formik } from 'formik';
+import { Form, Formik } from 'formik';
 import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { authRegister } from 'redux/auth/auth-operations';
-import {
-  Button,
-  Flex,
-  FormControl,
-  FormErrorMessage,
-  FormLabel,
-  Heading,
-  Input,
-  Stack,
-} from '@chakra-ui/react';
+import { Button, Flex, Heading, Stack } from '@chakra-ui/react';
+import AuthPasswordInput from 'components/Inputs/AuthPasswordInput';
+import AuthEmailInput from 'components/Inputs/AuthEmailInput';
+import AuthNameInput from 'components/Inputs/AuthNameInput';
 
 const RegistrationForm = () => {
   const dispatch = useDispatch();
+
+  const initialValues = {
+    name: '',
+    email: '',
+    password: '',
+  };
 
   const validateName = value => {
     if (!value) {
       return 'Name is required';
     }
+
+    if (value.length < 3) {
+      return 'Name should be at least 3 characters long';
+    }
+
+    if (value.length > 50) {
+      return 'Name should not exceed 50 characters';
+    }
   };
+
   const validateEmail = value => {
     if (!value) {
       return 'Email is required';
     }
+    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+
+    if (!emailRegex.test(value)) {
+      return 'Invalid email format';
+    }
   };
+
   const validatePassword = value => {
     if (!value) {
       return 'Password is required';
     }
+    if (value.length < 8) {
+      return 'Password should be at least 8 characters long';
+    }
+    if (value.length > 50) {
+      return 'Password should not exceed 50 characters';
+    }
   };
+
+  const handleSubmit = (value, actions) => {
+    dispatch(authRegister(value));
+    actions.setSubmitting(false);
+  };
+
   return (
-    <Flex direction="column" p={14} width="100%">
+    <Flex
+      direction="column"
+      p={14}
+      minH="calc(100vh - 42px)"
+      justify="center"
+      width="100%"
+    >
       <Heading size="xl" mb={10}>
         Create Account
       </Heading>
-      <Formik
-        initialValues={{
-          name: '',
-          email: '',
-          password: '',
-        }}
-        onSubmit={(value, actions) => {
-          setTimeout(() => {
-            dispatch(authRegister(value));
-            actions.setSubmitting(false);
-          }, 1000);
-        }}
-      >
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
         {props => (
           <Stack as={Form} gap={3}>
-            <Field type="text" name="name" validate={validateName}>
-              {({ field, form }) => (
-                <FormControl isInvalid={form.errors.name && form.touched.name}>
-                  <FormLabel>Name</FormLabel>
-                  <Input {...field} placeholder="Your Name" />
-                  <FormErrorMessage>{form.errors.name}</FormErrorMessage>
-                </FormControl>
-              )}
-            </Field>
-            <Field type="email" name="email" validate={validateEmail}>
-              {({ field, form }) => (
-                <FormControl
-                  isInvalid={form.errors.email && form.touched.email}
-                >
-                  <FormLabel>Email</FormLabel>
-                  <Input {...field} placeholder="your.email@mail.com" />
-                  <FormErrorMessage>{form.errors.email}</FormErrorMessage>
-                </FormControl>
-              )}
-            </Field>
-            <Field type="password" name="password" validate={validatePassword}>
-              {({ field, form }) => (
-                <FormControl
-                  isInvalid={form.errors.password && form.touched.password}
-                >
-                  <FormLabel>Password</FormLabel>
-                  <Input {...field} />
-                  <FormErrorMessage>{form.errors.password}</FormErrorMessage>
-                </FormControl>
-              )}
-            </Field>
-            <Flex justifyContent="space-evenly" alignItems="center">
+            <AuthNameInput validateName={validateName} />
+            <AuthEmailInput validateEmail={validateEmail} />
+            <AuthPasswordInput validatePassword={validatePassword} />
+
+            <Flex justifyContent="center" alignItems="center" gap="10" pt="5">
               <Button
-                textColor="fff"
                 colorScheme="blue"
                 isLoading={props.isSubmitting}
                 type="submit"
