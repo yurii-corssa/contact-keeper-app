cb73160f02a1b641ca107d363534552c59c440b7
yurii-corssa <yurii.corssa@gmail.com>
2023-09-24 11:46:33 +0200

    Create ModalWindow.jsx

---
+++ src/components/ModalWindow.jsx
@@ -0,0 +1,23 @@
+import {
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+} from '@chakra-ui/react';
+
+const ModalWindow = ({ isOpen, onClose, title, children }) => {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>{title}</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>{children}</ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default ModalWindow;
