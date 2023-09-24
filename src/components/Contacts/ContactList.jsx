3a1d41f9b43e451ba89c3644305ae0a42104cff5
yurii-corssa <yurii.corssa@gmail.com>
2023-09-24 11:49:44 +0200

    Enhance ContactList: Add modal, modularize notices

--- src/components/Contacts/ContactList.jsx
+++ src/components/Contacts/ContactList.jsx
@@ -1,5 +1,5 @@
-import { Box, Flex, Text } from '@chakra-ui/react';
-import { useEffect } from 'react';
+import { Box, Flex, useDisclosure } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getContactsThunk } from 'redux/contacts/contacts-operations';
 import { ContactItems } from './ContactItems';
@@ -7,39 +7,53 @@ import {
   selectContacts,
   selectVisibleContacts,
 } from 'redux/contacts/contacts-selectors';
+import NoContactNotice from './NoContactNotice';
+import NoFilterContactNotice from './NoFilterContactNotice';
+import ModalWindow from 'components/ModalWindow';
+import { EditContactForm } from './EditContactForm';
 
 const ContactsList = () => {
+  const [id, setId] = useState('');
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
   const contacts = useSelector(selectContacts);
   const filteredContacts = useSelector(selectVisibleContacts);
-
   const dispatch = useDispatch();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   useEffect(() => {
     dispatch(getContactsThunk());
   }, [dispatch]);
 
+  const handleOpenModal = (inputId, inputName, inputNumber) => {
+    setId(inputId);
+    setName(inputName);
+    setNumber(inputNumber);
+    onOpen();
+  };
+
   return (
-    <Flex direction="column" width="100%">
-      <Box h="calc(100vh - 42px)" overflowY="auto">
-        {contacts.length === 0 ? (
-          <Flex justifyContent="center" alignItems="center" height="100%">
-            <Text fontSize="lg" color="gray">
-              Your contact book is empty. Add your first contact!
-            </Text>
-          </Flex>
-        ) : filteredContacts.length === 0 ? (
-          <Flex justifyContent="center" alignItems="center" height="100%">
-            <Text fontSize="lg" color="gray">
-              No contacts found with that name.
-            </Text>
-          </Flex>
-        ) : (
-          <Box width="100%" overflowY="auto">
-            <ContactItems />
-          </Box>
-        )}
-      </Box>
-    </Flex>
+    <>
+      <Flex direction="column" width="100%">
+        <Box h="calc(100vh - 42px)" overflowY="auto">
+          {contacts.length === 0 ? (
+            <NoContactNotice />
+          ) : filteredContacts.length === 0 ? (
+            <NoFilterContactNotice />
+          ) : (
+            <ContactItems handleOpenModal={handleOpenModal} />
+          )}
+        </Box>
+      </Flex>
+      <ModalWindow isOpen={isOpen} onClose={onClose} title="Edit Contact">
+        <EditContactForm
+          id={id}
+          initialValues={{ name, number }}
+          onClose={onClose}
+        />
+      </ModalWindow>
+    </>
   );
 };
 
