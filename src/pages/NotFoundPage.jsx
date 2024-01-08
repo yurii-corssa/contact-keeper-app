1ef6d4bd907620eaa357ac428d27f1a477cf3f69
yurii-corssa <yurii.corssa@gmail.com>
2024-01-08 14:55:07 +0100

    fixed not found page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,16 +1,19 @@
-import { Box, Heading, Text, Button } from '@chakra-ui/react';
+import { Heading, Text, Button, Flex } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 import React from 'react';
 
 function NotFoundPage() {
   return (
-    <Box textAlign="center" mt="5rem">
-      <Heading mb={4}>404: Page Not Found</Heading>
+    <Flex justify="center" alignItems="center" h="100vh" flexDir="column">
+      <Heading size="4xl" mb={4}>
+        404
+      </Heading>
+      <Heading mb={4}>Page Not Found</Heading>
       <Text mb={4}>Sorry, the page you are looking for does not exist.</Text>
       <Button as={Link} to="/" colorScheme="blue">
         Go to Home
       </Button>
-    </Box>
+    </Flex>
   );
 }
 
